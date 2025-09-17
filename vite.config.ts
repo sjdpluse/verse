f9f6101f4b7cd67cf5fc619ff692f519ec7a5f99
Sajad Mohammadi <sajadalimohammadi394@gmail.com>
2025-09-17 07:31:08 +0500

    chore: update config for new repository

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/sjdverse/",
+  base: "/verse/",
   server: {
     host: "::",
     port: 8080,
